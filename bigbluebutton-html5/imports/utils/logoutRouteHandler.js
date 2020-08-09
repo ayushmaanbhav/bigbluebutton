1d17b88e5fa59887c40c8ac881b5edd8a28fc3ce
Amar Sharma <amarsharma.hacker@gmail.com>
2020-08-09 18:26:57 +0530

    recording playback changes

--- bigbluebutton-html5/imports/utils/logoutRouteHandler.js
+++ bigbluebutton-html5/imports/utils/logoutRouteHandler.js
@@ -2,13 +2,8 @@ import Auth from '/imports/ui/services/auth';
 
 const logoutRouteHandler = () => {
   Auth.logout()
-    .then((logoutURL = window.location.origin) => {
-      const protocolPattern = /^((http|https):\/\/)/;
-
-      window.location.href =
-        protocolPattern.test(logoutURL) ?
-          logoutURL :
-          `http://${logoutURL}`;
+    .then(() => {
+      window.location.href = 'https://enlite.in';
     });
 };
 
